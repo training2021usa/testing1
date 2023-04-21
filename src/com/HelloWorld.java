fba258b26d573a80a4d0f8e9a50e19f7f7ff6201
training2021usa <training2021usa@gmail.com>
2023-04-20 19:16:32 -0500

    commit2 by develop branch

--- src/com/HelloWorld.java
+++ src/com/HelloWorld.java
@@ -4,6 +4,10 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println( "Done on day one=============================");
+		
+		System.out.println( "Done on day 2 by develop branch=============================");
+		
+		
 
 	}
 
